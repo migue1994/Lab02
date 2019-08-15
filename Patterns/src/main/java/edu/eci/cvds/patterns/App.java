c70ae31e4024cb709759f989e3c55ad944dc1db2
migue1994 <43935670+migue1994@users.noreply.github.com>
2019-08-15 10:01:50 -0500

    Add files via upload

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,20 @@
+package edu.eci.cvds.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+	
+   
+	
+    public static void main( String[] args )
+    {
+	
+	System.out.println( "Hello World!");
+	System.out.println( "Hello "+args[0]+"!");
+	System.out.println( "Hello "+args[1]+"!");
+
+
+    }
+}
---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -0,0 +1,6 @@
+package edu.eci.cvds.patterns.shape;
+
+public class ShapeFactory{
+	
+	
+}
